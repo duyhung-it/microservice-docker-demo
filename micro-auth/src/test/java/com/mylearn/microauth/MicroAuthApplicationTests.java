800541424ea35173b91405a492e33377d0356528
hungnd2 <hungnd2@techasians.com>
2024-04-25 15:04:24 +0700

    new src micro-auth

---
+++ micro-auth/src/test/java/com/mylearn/microauth/MicroAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearn.microauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
